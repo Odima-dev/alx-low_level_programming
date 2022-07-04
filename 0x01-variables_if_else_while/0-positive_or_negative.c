2fdc9205f528e880ea65b0380ec760c252059779
Odima-dev <vincentodima22@gmail.com>
2022-07-04 16:43:41 -0700

    Positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
@@ -14,12 +15,10 @@ int main(void)
 		if (n > 0)
 		{
 			printf("%i is positive\n", n);
-		}
-		else if (n == 0)
+		} else if (n == 0)
 		{
 			printf("%i is zero\n", n);
-		}
-		else if (n < 0)
+		} else if (n < 0)
 		{
 			printf("%i is negative\n", n);
 		}
