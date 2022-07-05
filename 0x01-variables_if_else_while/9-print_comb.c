00ab565023b2bac56b7ec5be6662b496858d46db
Odima-dev <vincentodima22@gmail.com>
2022-07-04 18:16:31 -0700

    Possible combinations of single digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: alway returns 0
+ *
+ */
+int main(void)
+int main(void)
+{
+	int n = '0';
+
+	while (n <= '9')
+	{
+		putchar(n);
+		if (n != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}			
+			n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
