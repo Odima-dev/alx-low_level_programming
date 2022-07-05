040aac7a48734c178d014299d1ff90ed496c2f98
Odima-dev <vincentodima22@gmail.com>
2022-07-04 19:54:55 -0700

    Combination of 3 digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,36 +1,34 @@
 #include <stdio.h>
+
 /**
- * main - entry point
- *
- * Return: alway returns 0
- *
- */
+ *  *main - print all combinations of three different digits
+ *   *Return: Always 0 (Success)
+ *    */
+
 int main(void)
 {
-	int i
-	int j;
-	int k;
+		int one;
+			int ten;
+				int hundred;
 
-	for (i = 0; i <= 9; i++)
-	{
-		for (j = 0; j <= 9; j++)
-		{
-			for (k = 0; k <= 9; k++)
-			{
-				if (k > j && j > i)
-				{
-					putchar(i + '0');
-					putchar(j + '0');
-					putchar(k + '0');
-					if (i != 7 || j != 8 || k != 9)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
+					for (hundred = '0'; hundred <= '9'; hundred++) /*hundreds place*/
+							{
+										for (ten = (hundred + 1); ten <= '9'; ten++) /*tens=100s+1*/
+													{
+																	for (one = (ten + 1); one <= '9'; one++) /*ones*/
+																					{
+																										putchar(hundred);
+																														putchar(ten);
+																																		putchar(one);
+																																						if (hundred != '7' || ten != '8' || one != '9')
+																																											{
+																																																	putchar(',');
+																																																						putchar(' ');
+																																																										}
+																																									}
+																			}
+											}
+						putchar('\n');
+
+							return (0);
 }
