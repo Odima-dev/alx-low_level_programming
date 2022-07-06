32203aaf0a3ccc9af7afd0bb37fccd02bd81fbe8
Odima-dev <vincentodima22@gmail.com>
2022-07-06 09:33:53 -0700

    Print the n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * Print_times_table - print multiplication table up to n
+ * print_times_table - print multiplication table up to n
  * @n: the integer
  */
 void print_times_table(int n)
