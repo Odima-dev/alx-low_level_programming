e707827c1d2e0c9130eb531b24774a1b808e17cf
Odima-dev <vincentodima22@gmail.com>
2022-07-06 06:43:38 -0700

    Print every minute of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,22 +5,19 @@
  */
 void jack_bauer(void)
 {
-	int hours = 0;
-	int min = 0;
+	int i;
+	int j;
 
-	while (hours < 24)
+	for (i = 0; i <= 23; i++)
 	{
-		While(min < 60)
+		for (j = 0; j <= 59; j++)
 		{
-			_putchar('0' + (hours / 10));
-			_putchar('0' + (hours % 10));
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
 			_putchar(':');
-			_putchar('0' + (min / 10));
-			_putchar('0' + (min % 10));
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
 			_putchar('\n');
-			min++;
 		}
-		min = 0;
-		hours++;
 	}
 }
