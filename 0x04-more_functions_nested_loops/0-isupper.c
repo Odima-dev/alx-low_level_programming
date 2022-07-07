e3f1fbfe0ba634432137bd06ec6e87777be87b13
Odima-dev <vincentodima22@gmail.com>
2022-07-07 08:52:55 -0700

    Check for uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main"
+/**
+ * _isupper - to check for uppercase
+ * @c: character that is checked
+ * Return: either 1 or 0
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+
+	return (0);
+}
