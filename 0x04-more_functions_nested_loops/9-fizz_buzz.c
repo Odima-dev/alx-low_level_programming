1c4ae3965cc38e23dec45af1fc65e6d7c9230b22
Odima-dev <vincentodima22@gmail.com>
2022-07-08 16:21:44 -0700

    Fizz-buzz test;

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * main - prints 1 to 100
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			if (i % 3 == 0)
+			{
+				printf("Fizz");
+			}
+			if (i % 5 == 0)
+			{
+				printf("Buzz");
+			} 
+		}else
+			{
+				printf("%d", i);
+			}
+			if (i != 100)
+				printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
