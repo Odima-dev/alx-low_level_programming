126656ace7cd9ee0b322021a58c6a5c274757ed6
Odima-dev <vincentodima22@gmail.com>
2022-07-14 05:04:37 -0700

    Add two numbers

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,5 +12,8 @@ char *cap_string(char *s);
 char *rot13(char *s);
 char *leet(char *s);
 void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+char *add_strings(char *n1, char *n2, char *r, int r_index);
+void print_buffer(char *b, int size);
 
 #endif
