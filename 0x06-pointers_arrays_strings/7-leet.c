7c883fe6440e21bca29db41e7ec4a4164329f9a6
Odima-dev <vincentodima22@gmail.com>
2022-07-14 03:58:38 -0700

    Encode a string into 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,18 +8,21 @@
 
 char *leet(char *s)
 {
-	int a[11] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
-	int b[11] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+	char alphaArr[] = "a4A4e3E3o0O0t7T7l1L1";
 
-	int i, j;
-
-	for (j = 0; s[j] != '\0'; j++)
+	int i;
+	int j;
+	
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (i = 0; a[i] != '\0'; i++)
+		for (j = 0; alphaArr[j] != '\0'; j++)
 		{
-			if (s[j] == a[i])
-				s[j] = b[i];
+			if (s[i] == alphaArr[j])
+			{
+				s[i] = alphaArr[j + 1];
+				break;
+			}
 		}
 	}
-	return (s)
+	return (s);
 }
