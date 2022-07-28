893b008c57c8187d83c245aafd1467442c27d4a5
Odima-dev <vincentodima22@gmail.com>
2022-07-28 08:49:47 -0700

    Multiply two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,34 +1,98 @@
 #include "main.h"
 
 /**
- * main - does multiplication
- *  @argc: n arguments
- *  @argv: args
- *  Return: integer
+ * len - return string length
+ * @str: string
+ * Return: string length
  */
 
-int main(int argc, char *argv[])
+int len(char *str)
 {
-	unsigned long mul;
-	int i, j;
+	int len = 0;
 
-	if (argc != 3)
+	if (str != NULL)
 	{
-		printf("Error\n");
-		exit(98);
+		while (str[len])
+			len++;
 	}
-	for (i = 1; i < argc; i++)
+	return (len);
+}
+
+/**
+ * num_words - word count
+ * @str: the string
+ * Return: word count
+ */
+
+int num_words(char *str)
+{
+	int i = 0, words = 0;
+
+	while (i <= len(str))
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+		if ((str[i] != ' ') && (str[i] != '\0'))
 		{
-			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{
-				printf("Error\n");
-				exit(98);
-			}
+			i++;
+		}
+		else if (((str[i] == ' ') || (str[i] == '\0')) && i && (str[i - 1] != ' '))
+		{
+			words += 1;
+			i++;
+		}
+		else
+		{
+			i++;
 		}
 	}
-	mul = atol(argv[1]) *  atol(argv[2]);
-	printf("%lu\n", mul);
-	return (0);
+	return (words);
+}
+
+/**
+ * strtow - split spring to words
+ * @str: the string
+ * Return: pointer
+ */
+
+char **strtow(char *str)
+{
+	char **split;
+	int i, j = 0, temp = 0, size = 0, words = num_words(str);
+
+	if (words == 0)
+		return (NULL);
+	split = (char **)malloc(sizeof(char *) * (words + 1));
+	if (split != NULL)
+	{
+		for (i = 0; i <= len(str) && words; i++)
+		{
+			if ((str[i] != ' ') && (str[i] != '\0'))
+				size++;
+			else if (((str[i] == ' ') || (str[i] == '\0')) && i && (str[i - 1] != ' '))
+			{
+				split[j] = (char *)malloc(sizeof(char) * size + 1);
+				if (split[j] != NULL)
+				{
+					while (temp < size)
+					{
+						split[j][temp] = str[(i - size) + temp];
+						temp++;
+					}
+					split[j][temp] = '\0';
+					size = temp = 0;
+					j++;
+				}
+				else
+				{
+					while (j-- >= 0)
+						free(split[j]);
+					free(split);
+					return (NULL);
+				}
+			}
+		}
+		split[words] = NULL;
+		return (split);
+	}
+	else
+		return (NULL);
 }
