c2da22aab538009a5be8f6ff39db0f0b01d67920
Odima-dev <vincentodima22@gmail.com>
2022-08-15 07:17:08 -0700

    Return number of elements in a linked list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * list_len - print length of list
+ * @h: linked list
+ * Return: number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
