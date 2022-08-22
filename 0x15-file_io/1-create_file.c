6ff5ec1fb5dc35a0ddbfbbd55b90a8e108604edd
Odima-dev <vincentodima22@gmail.com>
2022-08-22 14:10:57 -0700

    Create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,13 +5,13 @@
  * @str: string
  * Return: length
  */
-int _strlen(char *str)
+size_t _strlen(char *str)
 {
-	int len;
+	size_t i;
 
-	for (len = 0; str[len] != '\0'; len++)
+	for (i = 0; str[i]; i++)
 		;
-	return (len);
+	return (1);
 }
 
 /**
@@ -23,31 +23,17 @@ int _strlen(char *str)
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	int n_wrote;
+	ssize_t len = 0;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
-
-	/* create with permissions if file doesn't exist, else truncate */
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
-
-	/* if nothing to write, just return newly created file */
-	if (!text_content)
-	{
-		close(fd);
-		return (1);
-	}
-
-	/* write */
-	n_wrote = write(fd, text_content, _strlen(text_content));
-	if (n_wrote == -1 || n_wrote != _strlen(text_content))
-	{
-		clode(fd);
-		return (-1);
-	}
-
+	if (text_content != NULL)
+		len = write(fd, text_content, _strlen(text_content));
 	close(fd);
+	if (len == -1)
+		return (-1);
 	return (1);
 }
